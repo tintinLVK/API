deb50756076a0b8431ebe98e6b466e7d642da22f
flojucv <66565404+flojucv@users.noreply.github.com>
2024-04-30 16:33:22 +0200

    merge

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controllerMusic = require('./../controller/Music');
 const {random} = require('./../controller/Music');
-const swaggerUi = require('swagger-ui-express');
 
 router.get("/", (req,res) => {
     res.status(200).json({success: "racine API"});
@@ -17,6 +16,5 @@ router.get('/music', controllerMusic.find);
 router.post('/music', controllerMusic.create);
 router.get('/music/random', random);
 router.get('/music/:id', controllerMusic.findById);
-router.get('/api-docs',swaggerUi.serve,swaggerUi.setup(apiSpec));
 
 module.exports = router;
