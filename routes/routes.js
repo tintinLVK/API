783018f4dda6083cd9faae8cd544f02d5a3f8985
flojucv <66565404+flojucv@users.noreply.github.com>
2024-04-30 16:25:42 +0200

    test

--- routes/routes.js
+++ routes/routes.js
@@ -11,11 +11,10 @@ router.post('/', (req, res) => {
     res.status(200).json({ success: 'Bravo'});
 });
 
+
 router.get('/music', controllerMusic.find);
 router.post('/music', controllerMusic.create);
 router.get('/music/random', random);
 router.get('/music/:id', controllerMusic.findById);
-
-
 
 module.exports = router;
