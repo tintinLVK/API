7b2200be2e8ba8a5d519ac42fb75dc9f8be6375c
unknown <wallerand.rodolphe62@gmail.com>
2024-04-30 16:30:55 +0200

    update

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controllerMusic = require('./../controller/Music');
 const {random} = require('./../controller/Music');
+const swaggerUi = require('swagger-ui-express');
 
 router.get("/", (req,res) => {
     res.status(200).json({success: "racine API"});
@@ -15,6 +16,7 @@ router.get('/music', controllerMusic.find);
 router.post('/music', controllerMusic.create);
 router.get('/music/random', random);
 router.get('/music/:id', controllerMusic.findById);
+router.get('/api-docs',swaggerUi.serve,swaggerUi.setup(apiSpec));
 
 
 
