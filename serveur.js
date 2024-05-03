580c78d256861fa8a8ad636c32fcc9f0c67483fd
Your Name <you@example.com>
2024-05-03 11:02:05 +0200

    fin back

--- serveur.js
+++ serveur.js
@@ -1,1 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const connexion = require("./DB/dbconnect");
+const app = express();
+const port = 3000;
+const version = "v1";
+const router = require("./routes/routes");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+const options = require("./swagger.json");
+const specs = swaggerJsdoc(options);
+const Music = require("./models/Music");
+const { SELECT } = require("sequelize/lib/query-types");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(`/api/${version}`, router);
+app.use(
+  `/api/${version}/api-docs`,
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
+
+connexion.sync().then(() => {
+  console.log("DBconnect est synchronisé");
+  app.listen(port, () => {
+    console.log("Example app listening on port ${port}");
+  });
+});
 
